33080e9513f5ece363c17aae274d7b265baf0996
Lincxx <bossco28@gmail.com>
2020-03-08 08:56:41 -0400

    finished the app

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ newBtn.addEventListener('click', () => {
   if (newText.value !== '') {
     const newTodo = document.createElement('li');
     newTodo.innerText = newText.value;
+    newTodo.classList.add("text-center", "p-3", "bg-white", "mt-4", "rounded", "shadow-100", "cursor-pointer", "hover:bg-green-500")
     pendingList.appendChild(newTodo);
     newText.value = '';
   } else {
@@ -23,17 +24,35 @@ newText.addEventListener('keyup', event => {
   }
 })
 
-const move = (element, destination) => {
+const move = (element, destination, convertTo) => {
+  element
   if (element.localName == 'li') {
+    if (convertTo === 'completed') {
+      element.classList.add(
+        "line-through",
+        "text-red-500",
+        "hover:bg-red-500",
+        "hover:text-white"
+      );
+      element.classList.remove("hover:bg-green-500")
+    } else {
+      element.classList.remove(
+        "line-through",
+        "text-red-500",
+        "hover:bg-red-500",
+        "hover:text-white"
+      );
+      element.classList.add("hover:bg-green-500")
+    }
     destination.appendChild(element);
   }
 };
 
 pendingList.addEventListener('click', event => {
-  console.log(event.target.localName);
-  move(event.target, completedList);
+
+  move(event.target, completedList, 'completed');
 });
 
 completedList.addEventListener('click', event => {
-  move(event.target, pendingList);
+  move(event.target, pendingList, 'pendings');
 });
